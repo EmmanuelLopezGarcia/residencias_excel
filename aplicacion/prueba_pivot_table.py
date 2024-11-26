5cc4d7c72cb1d71e37d4af3f0c45e2616a6c6654
EmmanuelLopezGarcia <emmanuel161805_@hotmail.com>
2024-11-26 12:17:00 -0600

    Commit en archivo second_conexion_bd.py con depuración de listas y dataframes para liberación de memoria y que ya tiene los dos filtros FechaAlta y FechaIn, además de estar listo para añadir hojas al libro y graficos

--- aplicacion/prueba_pivot_table.py
+++ aplicacion/prueba_pivot_table.py
@@ -16,6 +16,6 @@ df3[['Proveedor_Nombre', 'Vendedor_Nombre', 'Pedido', 'Negocio', 'Descripcion2']
 
 with pd.ExcelWriter('Archivo_ventas.xlsx', engine='openpyxl', mode='a') as writer:
 
-    #df2.to_excel(writer,sheet_name='Alta_FILES', index=False)
+    df2.to_excel(writer,sheet_name='Alta_FILES', index=False)
     df3.to_excel(writer,sheet_name='Proveedor', index=False)
 
